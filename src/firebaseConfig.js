1c7cfc2697800b319d670cc73c451a4cbab0574a
hussamkhatib <hussamkhatib20@gmail.com>
2022-07-21 22:19:59 +0530

    fix: firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const app = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyA2Ys6l0DZV6hphtQcW_ceHcWmOSE-6IVA",
+  authDomain: "firepad-another.firebaseapp.com",
+  projectId: "firepad-another",
+  databaseURL: "https://firepad-another-default-rtdb.firebaseio.com",
+  storageBucket: "firepad-another.appspot.com",
+  messagingSenderId: "183344666162",
+  appId: "1:183344666162:web:580d05b0310c7c43d0ce2b",
+};
 
 function initialize() {
   if (!firebase.apps.length) firebase.initializeApp(app);
