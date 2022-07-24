c165428430b38a4e3f7c2f9c5e2a33bba12811c2
hussamkhatib <hussamkhatib20@gmail.com>
2022-07-24 12:21:27 +0530

    init react-routerv6

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import { useState, useEffect } from "react";
-import MultiUserSandpack from "./MultiUserSandpack";
+import { useEffect } from "react";
 import initialize from "./firebaseConfig";
+import { Routes, Route } from "react-router-dom";
+import Home from "./view/Home";
 
 export default function App() {
-  const [input, setInput] = useState("");
-  const [name, setName] = useState(null);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setName(input);
-  };
-
   useEffect(() => {
     initialize();
   }, []);
 
-  if (name) return <MultiUserSandpack name={name} />;
-  else
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your Name :
-          <input
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-            required
-          />
-        </label>
-        <button type="submit">Enter </button>{" "}
-      </form>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
